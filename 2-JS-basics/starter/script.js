9763c6f90552c5deaca7f7d86268aa639564af17
mblaul <mattblaul@gmail.com>
2018-08-01 21:46:12 -0400

    Worked on operators

---
+++ 2-JS-basics/starter/script.js
@@ -0,0 +1,38 @@
+// console.log("Hello world!!!");
+
+// var firstName = "John";
+
+// // Undefined - declaration but no assignment
+// var job;
+// console.log(job); // undefined
+
+// job = "Teacher";
+// console.log(job); // Teacher
+
+//*****************************/
+// Type Coersion              //
+
+var firstName = "John";
+var age = 28;
+
+console.log(firstName + " " + age);
+
+var job, isMarried;
+job = "Teacher";
+isMarried = false;
+
+// Basic Operators
+
+var yearJohn = 2018 - 28;
+var yearMark = 2018 - 28;
+
+var johnOlder = yearJohn < yearMark;
+console.log(johnOlder);
+
+console.log(typeof johnOlder);
+console.log(typeof null); //wtf - object
+console.log(typeof undefined); //undefined
+var x;
+console.log(typeof x);
+
+//type
