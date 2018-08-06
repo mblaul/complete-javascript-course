c3e44da66e59e31114c475128d9f58791e5d9128
mblaul <mattblaul@gmail.com>
2018-08-05 22:35:24 -0400

    Learning about operator precedence

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -12,27 +12,48 @@
 //*****************************/
 // Type Coersion              //
 
-var firstName = "John";
-var age = 28;
+// var firstName = "John";
+// var age = 28;
 
-console.log(firstName + " " + age);
+// console.log(firstName + " " + age);
 
-var job, isMarried;
-job = "Teacher";
-isMarried = false;
+// var job, isMarried;
+// job = "Teacher";
+// isMarried = false;
 
-// Basic Operators
+// // Basic Operators
 
-var yearJohn = 2018 - 28;
-var yearMark = 2018 - 28;
+// var yearJohn = 2018 - 28;
+// var yearMark = 2018 - 28;
 
-var johnOlder = yearJohn < yearMark;
-console.log(johnOlder);
+// var johnOlder = yearJohn < yearMark;
+// console.log(johnOlder);
 
-console.log(typeof johnOlder);
-console.log(typeof null); //wtf - object
-console.log(typeof undefined); //undefined
-var x;
-console.log(typeof x);
+// console.log(typeof johnOlder);
+// console.log(typeof null); //wtf - object
+// console.log(typeof undefined); //undefined
+// var x;
+// console.log(typeof x);
 
-//type
+// *******************
+// Operator precedence
+// *******************
+var now = 2018;
+var yearJohn = 1989;
+var fullAge = 18;
+
+// Multiple operators
+var isFullAge = now - yearJohn >= fullAge;
+console.log(isFullAge);
+
+// Grouping
+var ageJohn = now - yearJohn;
+var ageMark = 35;
+var average = (ageJohn + ageMark) / 2;
+console.log(average);
+
+//Multiple assignments
+
+var x, y;
+x = y = (3 + 5) * 4 - 6; // 8 * 4 - 6 then 32 - 6 // 26
+console.log(x, y);
