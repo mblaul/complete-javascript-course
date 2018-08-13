208358fe0c558948f7f1d8e4f0a9a3f40974b1eb
mblaul <mattblaul@gmail.com>
2018-08-12 21:26:38 -0400

    Worked on understanding Execution Context, Scoping, and This!

--- 3-how-JS-works/starter/script.js
+++ 3-how-JS-works/starter/script.js
@@ -1,25 +1,8 @@
 ///////////////////////////////////////
 // Lecture: Hoisting
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 ///////////////////////////////////////
 // Lecture: Scoping
-
 
 // First scoping example
 
@@ -37,8 +20,6 @@ function first() {
     }
 }
 */
-
-
 
 // Example to show the differece between execution stack and scope chain
 
@@ -62,16 +43,44 @@ function third() {
 }
 */
 
-
-
 ///////////////////////////////////////
 // Lecture: The this keyword
 
+calculateAge(1985);
 
+function calculateAge(year) {
+	console.log(2016 - year);
+	console.log(this);
+}
+// Result: Window object
 
+var john = {
+	name: "John",
+	yearOfBirth: 1989,
+	calculateAge: function() {
+		// Method call
+		console.log(this);
+		console.log(2016 - this.yearOfBirth);
 
+		// function innerFunction() {
+		// 	// Regular function call
+		// 	console.log(this);
+		// }
+		// innerFunction();
+	}
+};
 
+john.calculateAge();
+// Result calculateAge:     John object
+// Result innerFunction:    Window object
 
+var mike = {
+	name: "Mike",
+	yearOfBirth: 1984
+};
 
+mike.calculateAge = john.calculateAge;
+mike.calculateAge();
 
-
+// Result: Mike object with mike's yearOfBirth and age calculated
+// The example shows that 'this' is assigned only when it's called
