98cc62a2af2a8deeb4d31b988c93bbba43159153
mblaul <mattblaul@gmail.com>
2018-08-14 20:45:32 -0400

    Added game state to game.

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -9,43 +9,50 @@ GAME RULES:
 
 */
 
-var scores, roundScore, activePlayer, dice;
+var scores, roundScore, activePlayer, dice, gamePlaying;
 
 init();
 
 document.querySelector(".btn-roll").addEventListener("click", function() {
-	// Do something
-	dice = Math.floor(Math.random() * 6) + 1;
+	if (gamePlaying) {
+		// Do something
+		dice = Math.floor(Math.random() * 6) + 1;
 
-	// Display the result
-	var diceDOM = document.querySelector(".dice");
-	diceDOM.style.display = "block";
-	diceDOM.src = "dice-" + dice + ".png";
+		// Display the result
+		var diceDOM = document.querySelector(".dice");
+		diceDOM.style.display = "block";
+		diceDOM.src = "dice-" + dice + ".png";
 
-	if (dice !== 1) {
-		roundScore += dice;
-		document.querySelector("#current-" + activePlayer).textContent = roundScore;
-	} else {
-		nextPlayer();
+		if (dice !== 1) {
+			roundScore += dice;
+			document.querySelector(
+				"#current-" + activePlayer
+			).textContent = roundScore;
+		} else {
+			nextPlayer();
+		}
 	}
 });
 
 document.querySelector(".btn-hold").addEventListener("click", function() {
-	scores[activePlayer] += roundScore;
-	document.querySelector("#score-" + activePlayer).textContent =
-		scores[activePlayer];
+	if (gamePlaying) {
+		scores[activePlayer] += roundScore;
+		document.querySelector("#score-" + activePlayer).textContent =
+			scores[activePlayer];
 
-	if (scores[activePlayer] >= 100) {
-		document.querySelector("#name-" + activePlayer).textContent = "Winner!";
-		document.querySelector(".dice").style.display = "none";
-		document
-			.querySelector(".player-" + activePlayer + "-panel")
-			.classList.add("winner");
-		document
-			.querySelector(".player-" + activePlayer + "-panel")
-			.classList.remove("active");
-	} else {
-		nextPlayer();
+		if (scores[activePlayer] >= 100) {
+			document.querySelector("#name-" + activePlayer).textContent = "Winner!";
+			document.querySelector(".dice").style.display = "none";
+			document
+				.querySelector(".player-" + activePlayer + "-panel")
+				.classList.add("winner");
+			document
+				.querySelector(".player-" + activePlayer + "-panel")
+				.classList.remove("active");
+			gamePlaying = false;
+		} else {
+			nextPlayer();
+		}
 	}
 });
 
@@ -55,6 +62,8 @@ function init() {
 	scores = [0, 0];
 	activePlayer = 0;
 	roundScore = 0;
+	gamePlaying = true;
+
 	document.querySelector(".dice").style.display = "none";
 
 	document.getElementById("score-0").textContent = 0;
